a35af3e88e5aa664b51430f33a507abb0b28b9e9
RusZh <ruszh96@users.noreply.github.com>
2016-11-11 16:40:09 +0600

    Doing Main page

--- app/controllers/AdminPageController.java
+++ app/controllers/AdminPageController.java
@@ -1,10 +1,10 @@
 package controllers;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import models.Approval;
 import models.Post;
 import models.User;
-import org.apache.commons.mail.HtmlEmail;
-import play.libs.Mail;
 import play.mvc.Controller;
 import repository.ApprovalRepository;
 import repository.Impl.ApprovalTestingRepositoryImpl;
@@ -12,8 +12,10 @@ import repository.Impl.PostTestingImpl;
 import repository.PostRepository;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 
 public class AdminPageController extends Controller{
@@ -41,26 +43,14 @@ public class AdminPageController extends Controller{
         AdminPageController.adminPage();
     }
     public static void uploadPhoto(String title, File photo) {
-
-        //For logging only
-        try{
-        HtmlEmail email = new HtmlEmail();
-        email.setFrom("me@hello.com");
-        email.addTo("me@hello.com");
-        email.setSubject("subject");
-        //This is the default text for clients that do not support HTML Message
-        email.setTextMsg("Not null  " + title + "   ");
-        email.setHtmlMsg("Not null  " + title + "   "+photo.getName());
-        //Play.libs.Mail
-        Mail.send(email);}
-        catch (Exception e){
-
+        Cloudinary cloudinary = new Cloudinary("CLOUDINARY_URL=cloudinary://297275137474391:2Dh-cCwMIig131jx9Vb-2r6sSMI@ruszh");
+        Map uploadResult = null;
+        try {
+            uploadResult = cloudinary.uploader().upload(photo, ObjectUtils.emptyMap());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        //For logging only
-
-        //System.out.println("Not null  " + title + "   ");
-        System.out.println("Not null  " + photo==null+ "   ");
-        //Logger.debug("Not null  " + title);
-        renderJSON("{\"error\":false,\"path\":\"http://www.mydomain.com/myimage.jpg\"}");// or {"error":"filetype"} or {"error":"unknown"}
+        String uploadURL= (String)uploadResult.get("url");
+        renderJSON("{\"error\":false,\"path\":\""+uploadURL+"\"}");// or {"error":"filetype"} or {"error":"unknown"}
     }
 }
