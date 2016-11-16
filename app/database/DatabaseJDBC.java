c4af70842e52663902f07a5d0419444543c07439
Alexey Muryshkin <Alexey Muryshkin@DESKTOP-K2GS3RD.mshome.net>
2016-11-17 01:49:39 +0600

    Updated Connection

--- app/database/DatabaseJDBC.java
+++ app/database/DatabaseJDBC.java
@@ -11,18 +11,18 @@ import play.Logger;
 
 public class DatabaseJDBC implements Database {
 
-	/*private static final String USER = "v56jxrify4fkgzze";
+	private static final String USER = "v56jxrify4fkgzze";
 	private static final String PASSWORD = "whu1t5b83x003cw5";
 	private static final String PORT = "3306";
 	private static final String SERVER = "vhw3t8e71xdz9k14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 	private static final String DATABASE = "dhrtt8tgflpynwmz";
-	private static final String DBMS = "mysql";*/
-	private static final String USER = "root";
+	private static final String DBMS = "mysql";
+	/*private static final String USER = "root";
 	private static final String PASSWORD = "123456789abc";
 	private static final String PORT = "3306";
 	private static final String SERVER = "localhost";
 	private static final String DATABASE = "bloggybase";
-	private static final String DBMS = "mysql";
+	private static final String DBMS = "mysql";*/
 	
 	private static DatabaseJDBC databaseInstance = null;
 	
