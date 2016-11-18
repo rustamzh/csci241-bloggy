52639829b56075cebc7b3137bcc53fa33f740564
RusZh <ruszh96@users.noreply.github.com>
2016-11-18 17:32:13 +0600

    Adding valid database info

--- app/database/DatabaseJDBC.java
+++ app/database/DatabaseJDBC.java
@@ -9,14 +9,10 @@ import java.util.Properties;
 
 public class DatabaseJDBC implements Database {
 
-	//private static final String USER = "r.zhumagambetov";
-	private static final String USER = "root";
-	//private static final String PASSWORD = "31NIHY1";
-	private static final String PASSWORD = "";
-	//private static final String PORT = "80";
-	private static final String PORT = "3306";
-	//private static final String SERVER = "46.101.171.158";
-	private static final String SERVER = "localhost";
+	private static final String USER = "r.zhumagambetov";
+	private static final String PASSWORD = "31NIHY1";
+	private static final String PORT = "80";
+	private static final String SERVER = "46.101.171.158";
 	private static final String DATABASE = "rustam_zhumagambetov";
 	private static final String DBMS = "mysql";
 	/*private static final String USER = "root";
@@ -44,12 +40,12 @@ public class DatabaseJDBC implements Database {
 	public Connection getConnection() {
 		if (conn == null) {
 			try {
-				//Connection temp = null;
+				Connection temp = null;
 				Properties connectionProps = new Properties();
 				connectionProps.put("user", USER);
 				connectionProps.put("password", PASSWORD);
-				conn= DriverManager.getConnection("jdbc:" + DBMS + "://" + SERVER + ":" + PORT + "/" + DATABASE, connectionProps);
-
+				temp = DriverManager.getConnection("jdbc:" + DBMS + "://" + SERVER + ":" + PORT + "/" + DATABASE, connectionProps);
+				conn = temp;
 			} catch (SQLException e) {
 				Logger.info("SQLException during database connection: " + e.getMessage());
 			}
