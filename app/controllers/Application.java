6dbf9cc8d697862405309fbb85261e2d1ae7ba4a
alexeymuryshkin <alexey.muryshkin@gmail.com>
2016-11-21 03:25:06 +0600

    Category - 1st Try

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,28 +16,42 @@ import java.util.List;
 import static controllers.AdminPageController.postRepository;
 
 public class Application extends Controller {
-
+	
 	static ApprovalRepository repo=new ApprovalTestingRepositoryImpl();
     static PostRepository postRepository = new PostRepositoryImpl();
-
+    
+    private static String curCategory;
+    
     @Before
     static void addDefaults() {
         renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
         renderArgs.put("blogLead", Play.configuration.getProperty("blog.lead"));
+        curCategory = "Uncategorized";
     }
 
     public static void index() {
         //PostRepository postRepository = new PostTestingImpl();
         //int number_of_likes, Date date, String body, String category, String user_nickname
 
-        List<Post> listposts = postRepository.getAllPosts();
-        int totalposts=listposts.size();
+        List<Post> listposts;
+        
+        if (curCategory == null || curCategory.isEmpty() || curCategory.equals("Uncategorized"))
+        	listposts = postRepository.getAllPosts();
+        else
+        	listposts = postRepository.getPostsbyCatgory(curCategory);
+        
+        //int totalposts=listposts.size();
         List<String> listCat = postRepository.getAllCategories();
         //listposts.add(0,featuredPost);
         ValuePaginator paginator = new ValuePaginator(listposts);
         paginator.setPageSize(5);
         
         User user = Security.getConnectedUser();
-        render(paginator, listCat, user);
+        render(paginator, listCat, user, curCategory);
+    }
+    
+    public static void setCategory(String category) {
+    	curCategory = category;
+    	index();
     }
 }
