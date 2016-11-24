2bf2a2b0f216b35ab80ff2dd663afd8ac22bc677
RusZh <ruszh96@users.noreply.github.com>
2016-11-24 11:36:26 +0600

    editor admin page func modified

--- app/repository/Impl/ApprovalRepositoryImpl.java
+++ app/repository/Impl/ApprovalRepositoryImpl.java
@@ -23,7 +23,7 @@ public class ApprovalRepositoryImpl implements ApprovalRepository {
 
         try{
             Statement stm = conn.createStatement();
-            ResultSet rs = stm.executeQuery("select a.approvalId, a.commentId, p.title, c.user_nickname, c.body from (approval as a join comment as c on a.commentId=c.commentId) join post as p on c.post_postId=p.postid ");
+            ResultSet rs = stm.executeQuery("select a.approvalId, a.commentId, p.title, c.user_nickname, c.body from (approval as a join comment as c on a.commentId=c.commentId) join post as p on c.post_postId=p.postid ORDER BY a.approvalId DESC");
             while(rs.next()){
                 //int approvalId, int commentId
                 list.add(new Approval(Integer.parseInt(rs.getString(1)),Integer.parseInt(rs.getString(2)),rs.getString(3),rs.getString(4),rs.getString(5)));
